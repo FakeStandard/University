1a6dbe02bd70b0bfdb6e27806de5b0fce57d439f
Riley Lin <75247434+FakeStandard@users.noreply.github.com>
2022-10-17 21:31:01 +0800

    Create GenericRepository.cs

---
+++ DAL/Repository/GenericRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using University.Data;
+
+namespace University.DAL.Repository
+{
+    public class GenericRepository<T> where T : class
+    {
+        internal SchoolContext context;
+        internal DbSet<T> dbSet;
+
+        public GenericRepository(SchoolContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+        public virtual IEnumerable<T> Get(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = "")
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter is not null)
+                query = query.Where(filter);
+
+            foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                query = query.Include(includeProperty);
+
+            if (orderBy is not null)
+                return orderBy(query).ToList();
+            else
+                return query.ToList();
+        }
+
+        public virtual T GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+        public virtual void Insert(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public virtual void Update(T entity)
+        {
+            dbSet.Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Delete(object id)
+        {
+            T entity = dbSet.Find(id);
+            Delete(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            if (context.Entry(entity).State == EntityState.Detached)
+                dbSet.Attach(entity);
+
+            dbSet.Remove(entity);
+        }
+    }
+}
